8b7371b264b24863cde3b500c31fba5400a39d31
David Mbagwu <davidmbagwu@gmail.com>
2024-05-30 21:06:46 -0500

    Pushed the base html to the Django web-frame and create the required html pages with their required URLs, views functions and identifiers. Changed the background watermark from the NSBE logo to the McNeese Horseman logo. Changed the main logo from McNeese torch to NSBE torch.

--- nsbe_project/stage/urls.py
+++ nsbe_project/stage/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='stage-index'),
-    path('about/', views.about, name='stage-about')
+    path('about/', views.about, name='stage-about'),
+    path('directory/', views.directory, name='stage-directory'),
+    path('events/', views.events, name='stage-events'),
+    path('help/', views.help, name='stage-help'),
+    path('points/', views.points, name='stage-points'),
+    path('profile/', views.profile, name='stage-profile')
 ]
--- nsbe_project/stage/views.py
+++ nsbe_project/stage/views.py
@@ -7,3 +7,18 @@ def index(request):
 
 def about(request):
     return render(request, 'stage/about.html')
+
+def directory(request):
+    return render(request, 'stage/directory.html')
+
+def events(request):
+    return render(request, 'stage/events.html')
+
+def help(request):
+    return render(request, 'stage/help.html')
+
+def points(request):
+    return render(request, 'stage/points.html')
+
+def profile(request):
+    return render(request, 'stage/profile.html')
