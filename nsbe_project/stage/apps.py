4fa0b9184840fab3a95ed864474fe13f7e5e553f
sp-muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-07-03 16:19:54 +0200

    Created Member model. Created MemberSignupForm. Implemented signup, login, and logout views.

--- nsbe_project/stage/apps.py
+++ nsbe_project/stage/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class StageConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'stage'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "stage"
--- nsbe_project/stage/urls.py
+++ nsbe_project/stage/urls.py
@@ -1,16 +1,15 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='stage-index'),
-    path('about/', views.about, name='stage-about'),
-    path('signup/', views.signup, name='stage-signup'),
-    path('directory/', views.directory, name='stage-directory'),
-    path('events/', views.events, name='stage-events'),
-    path('help/', views.help, name='stage-help'),
-    path('points/', views.points, name='stage-points'),
-    path('profile/', views.profile, name='stage-profile'),
-    path('login/', views.login_view, name='stage-login'),
-    path('logout/', views.logout_view, name="stage-logout")
+    path("", views.index, name="stage-index"),
+    path("about/", views.about, name="stage-about"),
+    path("signup/", views.signup, name="stage-signup"),
+    path("directory/", views.directory, name="stage-directory"),
+    path("events/", views.events, name="stage-events"),
+    path("help/", views.help, name="stage-help"),
+    path("points/", views.points, name="stage-points"),
+    path("profile/", views.profile, name="stage-profile"),
+    path("login/", views.login_view, name="stage-login"),
+    path("logout/", views.logout_view, name="stage-logout"),
 ]
